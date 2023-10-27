9a5bf3ddb6e71c40d72eac4dd9366f57e5e50346
Thibault Colcomb <thibault.colcomb@hotmail.com>
2023-10-27 10:03:01 +0200

    main calls ncurses menu

--- main.c
+++ main.c
@@ -1,9 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+#include <ncurses.h>
+#include <locale.h>
+
 
 #include "./src/mineboard/mineboard.h"
+#include "./src/UI/menu/menu.h"
 
 int main(){
+    setlocale(LC_ALL, ""); //Config environment for UTF-8
+
+    //Init ncurses with some options
+    initscr(); // Init ncurses
+    curs_set(0); //Disable cursor
+    start_color(); // Ability to use colors
+    noecho(); // Disable caractere echo
+    mousemask(BUTTON1_CLICKED, NULL); // Enable mouse click detection
+
+    //Init ncurses windows
+    WINDOW *boite = newwin(0, 0, 0, 0);
+    keypad(boite, TRUE); // Enable keypad special keys detection
+
+    // Vérifie la prise en charge des couleurs
+    if (!has_colors()) {
+        endwin();
+        printf("Your terminal does not support colors display.\n");
+        return 1;
+    }
+
+    while(1){
+        int nb_player = display_menu(boite);
+        if(nb_player == -1)
+            break;
+    }
+
+    endwin(); // Quit ncurses
+
+
+
+    return 0;
+}
+
+int main2(){
     
     gameboard_t *gb = create_gameboard(10, 10, 1);
 
@@ -25,9 +65,8 @@ int main(){
         // printf display
         printf("\n");
         for(int i = 0; i < gb->nb_rows; i++){
-            for(int j = 0; j < gb->nb_cols; j++){
+            for(int j = 0; j < gb->nb_cols; j++)
                 printf("%c ", display[i][j]);
-            }
             printf("\n");
         }
         printf("Cases revealed: %d\n", gb->nb_cases_revealed);
@@ -73,14 +112,13 @@ int main(){
     }
 
 
-        get_gameboard_display(gb, display);
-        // printf display
-        for(int i = 0; i < gb->nb_rows; i++){
-            for(int j = 0; j < gb->nb_cols; j++){
-                printf("%c ", display[i][j]);
-            }
-            printf("\n");
-        }
+    get_gameboard_display(gb, display);
+    // printf display
+    for(int i = 0; i < gb->nb_rows; i++){
+        for(int j = 0; j < gb->nb_cols; j++)
+            printf("%c ", display[i][j]);
+        printf("\n");
+    }
 
     return 0;
 }
